718634cb4308674589f369f4737069d02f66e69f
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-06-01 21:23:10 -0500

    Add: Authlayout ui

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
 function AuthLayout() {
-  return <div>AuthLayout</div>
+  return (
+    <div className="flex flex-col min-h-screen bg-[#F5F7FD]">
+      <Outlet />
+    </div>
+  )
 }
 
 export default AuthLayout
